031bb9f261291fe3f716d4765dd6542069622435
sq1devtest <88003922+sq1devtest@users.noreply.github.com>
2021-07-26 22:51:54 +0200

    Footer widget update

--- template-parts/footer/footer-widgets.php
+++ template-parts/footer/footer-widgets.php
@@ -7,9 +7,6 @@
 <div class="col-md-3 widget-area">
     <?php the_custom_logo(); ?>
     <?php get_template_part( 'template-parts/footer/footer-socials' ); ?>
-    <?php if ( is_active_sidebar( 'footer-1' ) ) { ?>
-        <?php dynamic_sidebar( 'footer-1' ); ?>
-    <?php } ?>
     <p class="footer-copy">Copyright &copy <?php echo date('Y'); ?>. All rights reserved. Developed by <a href="https://www.square1.io/" target="_blank" title="Square1">Square1</a> and powered by <a href="https://www.publisherplus.com/" target="_blank" title="PublisherPlus.com">PublisherPlus.com</a></p>
 </div>
 
